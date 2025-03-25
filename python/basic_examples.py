e5d1a74ab499bb2dba1c875c70cb31171668bf40
hobkak-jeon <nokdujon@gmail.com>
2025-03-25 20:02:31 +0700

    feat: examples about "for loops"

---
+++ python/basic_examples.py
@@ -0,0 +1,41 @@
+"""Module providing a function default dict."""
+
+import collections
+
+STR_ALPHABET = "abcdef"
+lst_chars = ["u", "a", "e", "c", "k", "i"]
+dic_grades = collections.defaultdict(int)
+dic_grades["A"] = 90
+dic_grades["B"] = 80
+dic_grades["C"] = 1
+
+
+# For loops
+for ch in STR_ALPHABET:
+    print(ch)
+
+for idx, val in enumerate(lst_chars):
+    print(idx, val)
+
+for key, val in dic_grades.items():
+    print(key, val)
+
+for countdown in 5, 4, 3, 2, 1, "Yeah!":
+    print(countdown)
+
+for x in [0, 1, 2]:
+    pass
+
+for _ in range(10):
+    print("Hi!")
+
+for x in range(6):
+    print(x)
+
+    if x == 7:
+        break
+else:
+    print("Finally finished!")
+
+_, b = range(2)
+print(b)
