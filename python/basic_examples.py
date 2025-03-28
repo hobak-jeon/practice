481e3ad15d87feb7980b333dd65df3cbfb445ca2
hobkak-jeon <nokdujon@gmail.com>
2025-03-28 17:03:02 +0700

    feat: examples of the "is" operator and the "==" operator

--- python/basic_examples.py
+++ python/basic_examples.py
@@ -1,6 +1,7 @@
 """Module providing basic examples for programming in python."""
 
 import collections
+import copy
 
 STR_ALPHABET = "abcdef"
 lst_chars = ["u", "a", "e", "c", "k", "i"]
@@ -69,3 +70,39 @@ keyword_arguments(
 keyword_only_arguments(1, 2, o=3)  # 1 2 3 10
 keyword_only_arguments(3, 4, o=5, p=11)  # 3 4 5 11
 # list.sort(*, key=None, reverse=False)
+
+
+# Differences between 'is' and  '=='.
+# The 'is' operator in Python is used to check if two variables point to the same object.
+# Unlike the '==' operator, which checks if the values of two objects are equal.
+# The 'is' operator goes one step further to ensure that they are, in fact, the exact same object.
+
+# Return a shallow copy of the list.
+org_lst = [1, 2, 3]
+lst_by_shallow_copy = org_lst.copy()
+lst_by_deep_copy = copy.deepcopy(org_lst)
+
+if org_lst is lst_by_shallow_copy:
+    # 'is' compares id(org_lst) with id(lst_by_shallow_copy).
+    print("same id.")
+else:
+    print("different id.")
+
+if org_lst == lst_by_shallow_copy:
+    # '==' compares value of 'org_lst' with value of 'lst_by_shallow_copy'.
+    print("same value.")
+else:
+    print("different value.")
+
+
+if org_lst is lst_by_deep_copy:
+    # 'is' compares id(org_lst) with id(lst_by_deep_copy).
+    print("same id.")
+else:
+    print("different id.")
+
+if org_lst == lst_by_deep_copy:
+    # '==' compares value of 'org_lst' with value of 'lst_by_deep_copy'.
+    print("same value.")
+else:
+    print("different value.")
