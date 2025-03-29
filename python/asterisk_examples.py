01fad446d56ced85a3a65ca9dc577319270c5e40
hobkak-jeon <nokdujon@gmail.com>
2025-03-29 18:25:38 +0700

    refactor: add type annotations

--- python/asterisk_examples.py
+++ python/asterisk_examples.py
@@ -1,6 +1,7 @@
 """Module providing asterisk examples."""
 
 from functools import reduce
+from typing import Any
 
 # 0. Multiplication and power operations.
 MUL = 2 * 3  # 6
@@ -8,9 +9,9 @@ POWER = 2**3  # 8
 
 
 # 1. Repeatedly extending the list-type containers.
-zeros_list = [0] * 5
-zeros_tuple = (0,) * 5
-vector_list = [[1, 2, 3]]
+zeros_list: list[int] = [0] * 5
+zeros_tuple: tuple[int] = (0,) * 5
+vector_list: list[list[int]] = [[1, 2, 3]]
 
 for idx, value in enumerate(vector_list * 3):
     print([(idx + 1) * e for e in value])
@@ -22,19 +23,19 @@ for idx, value in enumerate(vector_list * 3):
 # * (keyword-only arguments): dict
 
 
-def positional_arguments(i, j, *args: tuple) -> None:
+def positional_arguments(i: int, j: int, *args: tuple[int]) -> None:
     """2.1. Function providing an example for positional arguments (*args)."""
     print(i, j)  # 1 2
     print(args)  # (3, 4, 5)
 
 
-def keyword_arguments(q, r, **kwargs: dict) -> None:
+def keyword_arguments(q: int, r: int, **kwargs: dict[str, int]) -> None:
     """2.2. Function providing an example for keyword arguments (**kwargs)."""
     print(q, r)
     print(kwargs)
 
 
-def keyword_only_arguments(m, n, *, o, p=10) -> None:
+def keyword_only_arguments(m: int, n: int, *, o: int, p: int = 10) -> None:
     """2.3. Function providing an example for keyword-only arguments (*)."""
     # Arguments must be passed as keyword arguments after "*".
     print(m, n, o, p)
@@ -64,12 +65,12 @@ def product(*numbers: tuple[int]) -> int:
     return reduce(lambda x, y: x * y, numbers)
 
 
-primes = [2, 3, 5, 7, 11, 13]
+primes: list[int] = [2, 3, 5, 7, 11, 13]
 print(product(*primes))  # Returns 30030 <- (2, 3, 5, 7, 11, 13)
 print(product(primes))  # An only argument ([2, 3, 5, 7, 11, 13])
 
 
-def pre_process(**headers: dict[str, str]) -> None:
+def pre_process(**headers: dict[str, Any]) -> None:
     """_preprocess headers_
 
     Raises:
@@ -77,9 +78,9 @@ def pre_process(**headers: dict[str, str]) -> None:
         ValueError: _description_
         ValueError: _description_
     """
-    text_type = headers["Accept"]
-    content_length = headers["Content-Length"]
-    host = headers["Host"]
+    text_type: str = headers["Accept"]
+    content_length: int = int(headers["Content-Length"])
+    host: str = headers["Host"]
 
     if "https" not in host:
         raise ValueError("You must use SSL for http communication.")
@@ -91,7 +92,7 @@ def pre_process(**headers: dict[str, str]) -> None:
         raise ValueError("length should be less than 500.")
 
 
-headers_dict = {
+headers_dict: dict[str, Any] = {
     "Accept": "text/plain",
     "Content-Length": 348,
     "Host": "https://mingrammer.com",
@@ -99,7 +100,7 @@ headers_dict = {
 pre_process(**headers_dict)
 
 
-integers = [1, 2, 3, 4, 5, 6]
+integers: list[int] = [1, 2, 3, 4, 5, 6]
 
 # left side should be a tuple or a list.
 (*a,) = integers  # a = [1, 2, 3, 4, 5, 6]
