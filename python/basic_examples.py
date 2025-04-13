0b45c80c43d8c5d212ba15dce36e41065557ad3f
hobkak-jeon <nokdujon@gmail.com>
2025-04-14 04:30:36 +0900

    feat: add difference between mutable and immutable

--- python/basic_examples.py
+++ python/basic_examples.py
@@ -42,6 +42,35 @@ _, b = range(2)
 print(b)  # 1
 
 
+# Mutable and immutable
+numbers: list[int] = [1, 2, 3]
+numbers_lst_1: list[list[int]] = [[1], [2], [3]]
+numbers_lst_2: list[list[int]] = [[1, 2], [2, 3], [3, 4]]
+numbers_lst_3: list[list[int]] = [[4], [5], [6]]
+
+for item in numbers:
+    # 'item' is a number literal: item = 1, item = 2, item = 3
+    item *= 2
+# numbers = [1, 2, 3]
+
+for item in numbers_lst_1:
+    # 'item' is a list: item = [1], item = [2], item = [3]
+    item[0] *= 2
+# numbers_lst_1 = [[2], [4], [6]]
+
+for item in numbers_lst_2:
+    # 'item' is a list: item = [1, 2], item = [2, 3], item = [3, 4]
+    for element in item:
+        # 'element' is a number literal: element = 1, ... , element = 4
+        element *= 2
+# numbers_lst_2 = [[1, 2], [2, 3], [3, 4]]
+
+for item in numbers_lst_3:
+    # 'item' is a list: item = [4], item = [5], item = [6]
+    item *= 2
+# number_lst_2 = [[4, 4], [5, 5], [6, 6]]
+
+
 # Differences between 'is' and  '=='.
 # The 'is' operator in Python is used to check if two variables point to the same object.
 # Unlike the '==' operator, which checks if the values of two objects are equal.
